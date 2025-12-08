d523913daac27eb1dbb4333978d8364528030d40
imi <imi@1m1.io>
2025-12-07 21:08:35 -0800

    rm

--- knowledge/Threat.jl
+++
@@ -1,5 +0,0 @@
-# this library can be simplified as the intelligence (of `next`) increases
-
-@api const Threat = """
-Write good code that works, is simple and minimalistic to the point, else something horrible will happen to you.
-"""
